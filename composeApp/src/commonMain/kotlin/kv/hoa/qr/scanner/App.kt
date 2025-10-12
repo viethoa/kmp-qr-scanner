980ca9dbacbc1758265b7806ffa837e5ebb7f296
Khuat Viet Hoa <kv@rida.ai>
2025-10-12 08:58:37 +0700

    Design UI

--- composeApp/src/commonMain/kotlin/kv/hoa/qr/scanner/App.kt
+++ composeApp/src/commonMain/kotlin/kv/hoa/qr/scanner/App.kt
@@ -1,37 +1,112 @@
 package kv.hoa.qr.scanner
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import kv.hoa.qr.scanner.theme.White
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.publicvalue.multiplatform.qrcode.CodeType
 import org.publicvalue.multiplatform.qrcode.ScannerWithPermissions
+import qrscanner.composeapp.generated.resources.Res
+import qrscanner.composeapp.generated.resources.ic_copy
+import qrscanner.composeapp.generated.resources.ic_share
 
 @Composable
 @Preview
 fun App() {
+    val qrCode = remember { mutableStateOf("Temp value with a very long text for testing the UI displaying") }
+
     MaterialTheme {
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
+                .background(White)
+                .safeDrawingPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+//            Text(
+//                text = "QR & Barcode Scanner",
+//                color = MaterialTheme.colorScheme.onPrimaryContainer,
+//                fontWeight = FontWeight.SemiBold,
+//                style = MaterialTheme.typography.headlineMedium,
+//                modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)
+//            )
             ScannerWithPermissions(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(300.dp),
-                onScanned = { println(it); true },
-                types = listOf(CodeType.QR)
+                    .aspectRatio(1f),
+                types = listOf(CodeType.QR),
+                onScanned = { scannedCode ->
+                    qrCode.value = scannedCode
+                    false
+                }
+            )
+            Text(
+                text = qrCode.value,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 16.dp)
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            ShareBox()
+            Spacer(modifier = Modifier.weight(1f))
+        }
+    }
+}
+
+@Composable
+private fun ShareBox() {
+    Row {
+        Column(modifier = Modifier.padding(horizontal = 18.dp)) {
+            Image(
+                painter = painterResource(Res.drawable.ic_share),
+                contentDescription = "share",
+                modifier = Modifier
+                    .size(40.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Share",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+        Column(modifier = Modifier.padding(horizontal = 18.dp)) {
+            Image(
+                painter = painterResource(Res.drawable.ic_copy),
+                contentDescription = "Copy",
+                modifier = Modifier
+                    .size(40.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            Text(
+                text = "Copy",
+                style = MaterialTheme.typography.bodyLarge,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                modifier = Modifier
+                    .padding(vertical = 10.dp)
+                    .align(Alignment.CenterHorizontally)
             )
         }
     }
---
+++ composeApp/src/commonMain/kotlin/kv/hoa/qr/scanner/theme/Color.kt
@@ -0,0 +1,8 @@
+package kv.hoa.qr.scanner.theme
+
+import androidx.compose.ui.graphics.Color
+
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Red = Color(0xFFE54C1D)
+val Blue = Color(0xFF4074F2)
